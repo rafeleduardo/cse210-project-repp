c9a3d1360c5dd0e776c97d9da6c86250214b1a27
Rafael Parra <rparra@byupathway.edu>
2025-07-03 18:37:32 +0200

    Week 2 Abstraction

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,14 @@
+﻿namespace Resumes;
+
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine("{0} ({1}) {2}-{3}", _jobTitle, _company, _startYear, _endYear);
+    }
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using Resumes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Architect";
+        job1._company = "Microsoft";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+        
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Intel";
+        job2._startYear = 2018;
+        job2._endYear = 2021;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Rafael Parra";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        
+        resume1.DisplayResume();
     }
 }
