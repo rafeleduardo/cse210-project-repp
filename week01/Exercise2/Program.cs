1dad2bab009adf876f1223a0f6b6c540ba7c8a9c
Rafael Parra <rparra@byupathway.edu>
2025-07-03 17:46:04 +0200

    Exercise 2 solution

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage (0-100)? ");
+        string gradeString = Console.ReadLine();
+        int grade = 0;
+        string letter;
+        string sign = "";
+        
+
+        if (gradeString != null)
+        {
+            grade = int.Parse(gradeString);
+            if (grade is > 97 or < 60)
+                sign = "";
+            else if (grade % 10 >= 7)
+                sign = "+";
+            else if (grade % 10 < 3)
+                sign = "-";
+            else
+                sign = "";
+        }
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";;
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        Console.WriteLine("Your grade is {0}{1}.", letter, sign);
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You approved the course!");
+        }
+        else
+        {
+            Console.WriteLine("Prepare yourself for the next course!");
+        }
     }
 }
