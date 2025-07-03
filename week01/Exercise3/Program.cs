1213f4531930f2ed5d4a20a68203a6209a0856e6
Rafael Parra <rparra@byupathway.edu>
2025-07-03 17:46:22 +0200

    Exercise 3 solution

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // -- Parts 1 and 2 --
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        
+        // --- Part 3 ---
+        Random generator = new Random();
+        
+        string playAgain = "Y";
+        
+        while(playAgain == "Y")
+        {
+            int magicNumber = generator.Next(1, 100);
+            int guessCount = 0;
+            
+            while (true)
+            {
+                Console.Write("What is your guess? ");
+                int guessNumber = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (guessNumber == magicNumber)
+                    break;
+
+                if (guessNumber > magicNumber)
+                    Console.WriteLine("Lower");
+                else
+                    Console.WriteLine("Higher");
+            }
+
+            Console.WriteLine("You guessed it!!");
+            Console.WriteLine("You did it in {0} guesses.", guessCount);
+
+            Console.Write("\nDo you want to play again? (Y=yes, N=no) ");
+            playAgain = Console.ReadLine();
+        }
+        
+        Console.WriteLine("Thank you for playing!");
     }
 }
