3160b7f873ab720778cf39b5c96896c196129f8a
Rafael Parra <rparra@byupathway.edu>
2025-07-16 07:26:41 +0200

    Fraction class set up.
    W03 Learning Activity: Encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,23 @@
-using System;
+using Fractions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f = new Fraction();
+        Console.WriteLine(f.GetFractionString());
+        Console.WriteLine(f.GetDecimalValue());
+        
+        Fraction f1 = new Fraction(5);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+        
+        Fraction f2 = new Fraction(3, 4);
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+        
+        Fraction f3 = new Fraction(1, 3);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
     }
 }
