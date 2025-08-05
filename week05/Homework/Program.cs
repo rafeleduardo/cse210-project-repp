1bb5543ba88e87d9e4a66a10d53ff62104ead1da
Rafael Parra <rparra@byupathway.edu>
2025-08-06 01:32:53 +0200

    Homework implementation

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using Homework;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+        
+        MathAssignment assignment1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(assignment1.GetHomeworkList());
+        Console.WriteLine();
+        
+        WritingAssignment assignment2 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetWritingInformation());
+        
     }
 }
---
+++ week05/Homework/WritingAssignment.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework;
+
+public class WritingAssignment : Assignment
+{
+    private string _title;
+    
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        string studentName = GetStudentName();
+        return _title + " by " + studentName;
+    }
+}
