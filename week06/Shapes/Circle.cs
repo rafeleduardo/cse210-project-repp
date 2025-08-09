2d4ba5d66999d94057682cb9c4e5a9e8ad73d467
Rafael Parra <rparra@byupathway.edu>
2025-08-09 23:50:46 +0200

    Week 06 Shapes

---
+++ week06/Shapes/Circle.cs
@@ -0,0 +1,16 @@
+﻿namespace Shapes;
+
+public class Circle : Shape
+{
+    private double _radius;
+    
+    public Circle(string color, double radius) : base(color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,22 @@
-using System;
+using Shapes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square("Red", 3.0);
+        shapes.Add(square);
+        
+        Rectangle rectangle = new Rectangle("Blue", 3.0, 2.0);
+        shapes.Add(rectangle);
+        
+        Circle circle = new Circle("Yellow", 2.0);
+        shapes.Add(circle);
+        
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has a calculated area of {shape.GetArea():F2}");
+        }
     }
 }
